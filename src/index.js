aba14eb67dfe7f50528341985940cea5e4d25c26
Nate <nathanaelsgibson@gmail.com>
2020-12-23 03:08:56 -0800

    small update to save progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /*
 TODO:
-- restyle
+- fix edge and one endpoint selected hits edge of canvas [with new code]. (deselect edge?)
+- custom checkbox css
 - Fix isBipartite?
 - Fix edge arrow and parallel edge graphics
 - Way to graphically handle a lot of loops
@@ -66,24 +67,24 @@ const VERTEX_COLOR_6 = 'LightSalmon';
         document.getElementById('vertex_ids').addEventListener('change', toggleDisplayVertexIds);
         document.getElementById('vertex_degrees').addEventListener('change', toggleDisplayVertexDegrees);
 
-        canvas.on('object:moving', function (e) {
-            var obj = e.target;
-            // if object is too big ignore
-            if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
-              return;
-            }
-            obj.setCoords();
-            // top-left  corner
-            if(obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0){
-              obj.top = Math.max(obj.top, obj.top-obj.getBoundingRect().top);
-              obj.left = Math.max(obj.left, obj.left-obj.getBoundingRect().left);
-            }
-            // bot-right corner
-            if(obj.getBoundingRect().top+obj.getBoundingRect().height  > obj.canvas.height || obj.getBoundingRect().left+obj.getBoundingRect().width  > obj.canvas.width){
-              obj.top = Math.min(obj.top, obj.canvas.height-obj.getBoundingRect().height+obj.top-obj.getBoundingRect().top);
-              obj.left = Math.min(obj.left, obj.canvas.width-obj.getBoundingRect().width+obj.left-obj.getBoundingRect().left);
-            }
-        });
+        // canvas.on('object:moving', function (e) {
+        //     var obj = e.target;
+        //     // if object is too big ignore
+        //     if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
+        //       return;
+        //     }
+        //     obj.setCoords();
+        //     // top-left  corner
+        //     if(obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0){
+        //       obj.top = Math.max(obj.top, obj.top-obj.getBoundingRect().top);
+        //       obj.left = Math.max(obj.left, obj.left-obj.getBoundingRect().left);
+        //     }
+        //     // bot-right corner
+        //     if(obj.getBoundingRect().top+obj.getBoundingRect().height  > obj.canvas.height || obj.getBoundingRect().left+obj.getBoundingRect().width  > obj.canvas.width){
+        //       obj.top = Math.min(obj.top, obj.canvas.height-obj.getBoundingRect().height+obj.top-obj.getBoundingRect().top);
+        //       obj.left = Math.min(obj.left, obj.canvas.width-obj.getBoundingRect().width+obj.left-obj.getBoundingRect().left);
+        //     }
+        // });
     }
 
     // Updates UI.
